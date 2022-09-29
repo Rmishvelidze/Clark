701b0de3fc0b36584c0abc91b7c3fef81e5cc44e
Rezo Mishvelidze <rmishvelidze@credo.ge>
2022-09-29 17:45:56 +0400

    Fix failure unit test

--- Ir.Interview/Products/ProductsService.cs
+++ Ir.Interview/Products/ProductsService.cs
@@ -1,43 +1,46 @@
 ﻿namespace Ir.Interview
 {
-  public class ProductsService
-  {
-    #region Do Not Change
-    
-    private readonly List<Product> _products;
-
-    public ProductsService()
+    public class ProductsService
     {
-      _products = new List<Product>();
-      for (int i = 0; i < 40000; i++)
-      {
-        var id = i;
-        _products.Add(new Product
+        #region Do Not Change
+
+        private readonly List<Product> _products;
+
+        public ProductsService()
         {
-          Id = id,
-          Name = $"Shoe {id}"
-        });
-      }
-      _products.Shuffle();
-    }
-
-    public IReadOnlyList<Product> Products
-    {
-      get { return _products.AsReadOnly(); }
-    } 
-    #endregion
-
-    public IEnumerable<Product> UpdatePrices(IEnumerable<ProductPrice> newPrices)
-    {
-      foreach (var product in _products)
-      {
-        var newPrice = newPrices.SingleOrDefault(productWithPrice => productWithPrice.ProductId == product.Id);
-        if (newPrice != null)
-        {
-          product.Price = newPrice.Price;
+            _products = new List<Product>();
+            for (int i = 0; i < 40000; i++)
+            {
+                var id = i;
+                _products.Add(new Product
+                {
+                    Id = id,
+                    Name = $"Shoe {id}"
+                });
+            }
+            _products.Shuffle();
         }
-      }
-      return _products;
+
+        public IReadOnlyList<Product> Products
+        {
+            get { return _products.AsReadOnly(); }
+        }
+        #endregion
+
+        public IEnumerable<Product> UpdatePrices(IEnumerable<ProductPrice> newPrices)
+        {
+            var orderedNewPrices =  newPrices.OrderBy(x => x.ProductId).ToList();
+            var orderedProducts = _products.OrderBy(x => x.Id).ToList();
+
+            for (int i = 0; i < orderedProducts.Count; i++)
+            {
+                if(orderedProducts[i].Id  == orderedNewPrices[i].ProductId)
+                {
+                    orderedProducts[i].Price = orderedNewPrices[i].Price;
+                }
+            }
+
+            return orderedProducts;
+        }
     }
-  }
 }
